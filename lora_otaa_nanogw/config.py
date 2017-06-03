43b7ab8aa282d549708f6329c72db60aafd046d7
NiekB4 <niek_bebelaar@hotmail.com>
2017-06-03 19:04:23 +0200

    small changes

--- lora_otaa_nanogw/config.py
+++ lora_otaa_nanogw/config.py
@@ -2,6 +2,6 @@
 Change these for your own keys (see https://www.thethingsnetwork.org)
 Type has to be string
 """
-DEV_EUI = '70B3D54998B3B0D7'
-APP_EUI = '70B3D57EF0004E74'
-APP_KEY = '63B8BFCB5BACFE93EBE792775C26F829'
+DEV_EUI = '<..>'
+APP_EUI = '<..>'
+APP_KEY = '<..>'
